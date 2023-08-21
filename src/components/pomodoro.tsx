b7fa52d3c13abc5eb01255c88829eff74cf8e45a
andreluismoraes500 <124262969+andreluismoraes500@users.noreply.github.com>
2023-08-21 10:59:12 -0300

    ajustando a tela inicial para trabalho e intervano, criando component pomodoro, movendo o customHook para pasta hook

---
+++ src/components/pomodoro.tsx
@@ -0,0 +1,52 @@
+import { usePomodoro } from "../hooks/customHook";
+
+type propsPomodoro = {
+  value: number;
+};
+
+const Pomodoro = ({ value }: propsPomodoro) => {
+  const { timer, isActive, start, pause, reset, stop } = usePomodoro(value);
+
+  const handleStart = () => {
+    /**start pomodoro */
+    start();
+  };
+  const handlePause = () => {
+    /**pause pomodoro */
+    pause();
+  };
+  const handleRefresh = () => {
+    reset();
+    stop();
+  };
+
+  return (
+    <div className="font-press flex justify-center items-center select-none flex-col">
+      <div className=" text-red-300  text-7xl">{timer}</div>
+      <div className="flex gap-4 mt-6">
+        <button
+          onClick={handleStart}
+          disabled={isActive}
+          className="text-blue-300 bg-blue-900 text-3xl rounded p-2 flex justify-center items-center hover:bg-blue-600"
+        >
+          Start
+        </button>
+        <button
+          disabled={!isActive}
+          onClick={handlePause}
+          className="text-blue-300 bg-blue-900 text-3xl rounded p-2 flex justify-center items-center hover:bg-blue-600"
+        >
+          Pause
+        </button>
+        <button
+          onClick={handleRefresh}
+          className="text-blue-300 bg-blue-900 text-3xl rounded p-2 flex justify-center items-center hover:bg-blue-600"
+        >
+          Refresh
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Pomodoro;
--- src/hooks/customHook.tsx
+++ src/hooks/customHook.tsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import audio from "./audio/Keep It Going.mp3";
+import audio from "../audio/Keep It Going.mp3";
 
-export const usePomodoro = (initialTime: number) => {
-  const [timer, setTimer] = useState(initialTime);
-  const [isActive, setIsActive] = useState(false);
-  const [play, { stop }] = useSound(audio);
+type TimerControl = {
+  timer: string;
+  isActive: boolean;
+  start: () => void;
+  pause: () => void;
+  reset: () => void;
+  stop: () => void;
+};
+
+export const usePomodoro = (initialTime: number): TimerControl => {
+  const [timer, setTimer] = useState<number>(initialTime);
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [play, { stop }] = useSound<string>(audio);
 
   useEffect(() => {
     let interval: ReturnType<typeof setInterval> | undefined;
 
     if (isActive && timer > 0) {
       interval = setInterval(() => {
-        setTimer((prevTimer) => prevTimer - 1);
+        setTimer((prevTimer: number) => prevTimer - 1);
       }, 1000);
     } else if (!isActive && timer !== 0) {
       clearInterval(interval);
